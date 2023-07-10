aa782562081d60c9c25a30f1f5ff84623eb07e74
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-07-10 20:23:02 +0200

    feat: search functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Search = () => {
-  return <div>Search</div>;
+const Search = ({ query, setQuery }) => {
+  return (
+    <div>
+      Search:
+      <input
+        value={query}
+        type='search'
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </div>
+  );
+};
+
+Search.propTypes = {
+  query: PropTypes.string,
+  setQuery: PropTypes.func,
 };
 
 export default Search;
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -10,15 +10,20 @@ const apiProducts = fetchData(
 
 const ProductListPage = () => {
   const products = apiProducts.read();
+  const [query, setQuery] = React.useState('');
+
+  const filteredProducts = products.filter((product) =>
+    product.model.toLowerCase().includes(query.toLowerCase())
+  );
 
   return (
     <div>
       Product List
       <Header />
-      <Search />
+      <Search query={query} setQuery={setQuery} />
       <Item />
       <Suspense fallback={<div>Loading...</div>}>
-        {products?.map((product) => (
+        {filteredProducts?.map((product) => (
           <li key={product.id}>{product.model}</li>
         ))}
       </Suspense>
