a377843214b6df4a1b9d3064f49983985c31dc59
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-07-11 13:07:38 +0200

    feat: add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
-import ProductListPage from './pages/ProductListPage';
-import ProductDetailsPage from './pages/ProductDetailsPage';
+
+// Routing
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from 'react-router-dom';
+
+//Pages
+import ProductListPage from './pages/ProductListPage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
+import NotFound from './pages/NotFound';
 import RootLayout from './layouts/RootLayout';
 
 const router = createBrowserRouter(
@@ -14,6 +19,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<RootLayout />}>
       <Route path='/' element={<ProductListPage />} />
       <Route path='/product' element={<ProductDetailsPage />} />
+      <Route path='*' element={<NotFound />} />
     </Route>
   )
 );
